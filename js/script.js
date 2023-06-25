b702886575410cb98a8b57b3b27077d58a7ea151
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-25 18:09:52 -0300

    feat: função para adicionar o remover classe 'light'

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle('light') // Verifica se tem o 'light', se não tiver ele coloca, se tiver ele tira
+
+    const image = document.querySelector('#profile img')
+
+    if (html.classList.contains('light')) {
+        // Se estiver com a classe 'light', ele irá adicionar a imagem para versão light
+        image.setAttribute('src', '/assets/avatar-light.png');
+    }
+    else {
+        // Se estiver sem a classe 'light', ele irá adicionar a imagem para a versão black
+        image.setAttribute('src', '/assets/foto.png')
+    }
+
+
+}
